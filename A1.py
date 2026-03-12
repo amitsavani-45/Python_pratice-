4b51958898a2298186ae980f3f8089d5b73038ca
amitsavani-45 <apsavani4518@gmail.com>
2026-03-12 22:16:55 +0530

    assignment1

---
+++ A1.py
@@ -0,0 +1,24 @@
+"""
+Who developed Python?
+→ Guido van Rossum developed Python.
+
+When was the first version of Python launched?
+→ The first version of Python was released in 1991.
+
+"Python is a platform independent language" – True or False
+→ True 
+Python programs can run on different operating systems like Windows, Linux, and macOS without changing the code.
+
+How to make a single line comment in a Python file?
+→ Use the # symbol.
+Example:
+
+# This is a single line comment
+print("Hello")
+
+"Python is a statically typed programming language" – True or False
+→ False 
+Python is a dynamically typed language.
+
+
+"""
